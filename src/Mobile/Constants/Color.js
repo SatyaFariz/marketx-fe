3cc9314c37f93f46fdf3f0a5ff3f3678d1730c9c
Stanley <satyafariznur@gmail.com>
2021-07-24 00:16:09 +0700

    test

--- src/Mobile/Constants/Color.js
+++ src/Mobile/Constants/Color.js
@@ -1,3 +1,4 @@
 module.exports = {
-  primary: '#55aded'//'#1BA0E2'
+  primary: '#55aded',//'#1BA0E2'
+  link: 'rgb(27, 149, 224)'
 }
--- src/Mobile/Screens/Login/LoginScreen.js
+++ src/Mobile/Screens/Login/LoginScreen.js
@@ -1,10 +1,69 @@
+import { LOGO_URL } from '../../Constants'
+import { TextField, Button } from '@material-ui/core'
+import Color from '../../Constants/Color'
+import Link from '../../Components/Link'
 
 const Component = props => {
   return (
     <div style={{
-      height: 20000
+      paddingTop: 20,
+      paddingBottom: 20,
+      paddingLeft: 30,
+      paddingRight: 30
     }}>
-      Login
+      <img
+        alt="twitter"
+        src={LOGO_URL}
+        style={{
+          height: 38,
+          width: 38
+        }}
+      />
+
+      <h1 style={{
+        marginTop: 30,
+        marginBottom: 10
+      }}>Log in to RentX</h1>
+
+      <TextField
+        variant="outlined"
+        label="Phone Number"
+        fullWidth
+        style={{
+          marginTop: 10,
+          marginBottom: 10
+        }}
+      />
+
+      <Button
+        variant="contained"
+        style={{
+          marginTop: 10,
+          marginBottom: 10,
+          textTransform: 'none',
+          height: 44
+        }}
+        disableElevation
+        fullWidth
+      >
+        Log in
+      </Button>
+
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        width: '100%',
+        justifyContent: 'center',
+        paddingTop: 20
+      }}>
+        <Link
+          href='/register'
+          style={{ color: Color.link }}
+        >
+        <span>Create an account</span>
+        </Link>
+        
+      </div>
     </div>
   )
 }
