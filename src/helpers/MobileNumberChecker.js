97f2a46b7e5238bc2eb43c0cd2246a596716f426
Stanley <satyafariznur@gmail.com>
2021-07-26 01:52:38 +0700

    add MobileNumberChecker class

---
+++ src/helpers/MobileNumberChecker.js
@@ -0,0 +1,51 @@
+import { fetchQuery, graphql } from 'react-relay'
+
+const query = graphql`
+  query MobileNumberCheckerQuery($mobileNumber: String!) {
+    mobileNumberExists(mobileNumber: $mobileNumber)
+  }
+`
+
+var cache = {}
+
+class MobileNumberChecker {
+  constructor(environment) {
+    this.environment = environment
+    this.waitingFor = null
+  }
+
+  setCache(key, val) {
+    cache[key] = val
+  }
+
+  getCache(key) {
+    return cache[key]
+  }
+
+  checkExistance(mobileNumber, cb) {
+    this.waitingFor = mobileNumber
+    const cached = this.getCache(mobileNumber)
+
+    if(mobileNumber.trim() === '')
+      return
+    else if(cached) {
+      cb(cached)
+    } else {
+      fetchQuery(this.environment, query, { mobileNumber }).toPromise().then(data => {
+        const exists = data.mobileNumberExists
+        if(this.waitingFor === mobileNumber)
+          cb({
+            mobileNumber: this.waitingFor,
+            exists
+          })
+
+        this.setCache(mobileNumber, {
+          mobileNumber,
+          exists
+        })
+      })
+    }
+  }
+}
+
+export default MobileNumberChecker
