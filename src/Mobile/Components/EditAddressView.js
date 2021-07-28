650d5e2e7245a9720f78a1151b695746b81c9fb3
Stanley <satyafariznur@gmail.com>
2021-07-28 13:48:24 +0700

    add mapview

---
+++ src/Mobile/Components/EditAddressView.js
@@ -0,0 +1,29 @@
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+))
+
+const Component = props => {
+  return (
+    <div style={{
+      height: '100%',
+      backgroundColor: 'pink'
+    }}>
+      <MyMapComponent 
+        isMarkerShown={false} 
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
+  )
+}
+
+export default Component
--- src/Mobile/Screens/Store/StoreScreen.js
+++ src/Mobile/Screens/Store/StoreScreen.js
@@ -1,8 +1,64 @@
+import EditAddressView from '../../Components/EditAddressView'
+import { HEADER_HEIGHT, HEADER_BORDER_BOTTOM_COLOR, DIVIDER_COLOR } from '../../Constants'
+import { TextField, Button } from '@material-ui/core'
+import { useState, useRef, useEffect } from 'react'
+import useAppContext from '../../hooks/useAppContext'
+import { IoChevronBackSharp } from 'react-icons/io5'
 
 const Component = props => {
+  const { history } = useAppContext()
   return (
-    <div>
+    <div style={{
+      backgroundColor: 'pink',
+      height: '100%'
+    }}>
+      <div style={{
+        height: HEADER_HEIGHT,
+        backgroundColor: 'white',
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        position: 'absolute',
+        top: 0,
+        borderBottom: `1px solid ${HEADER_BORDER_BOTTOM_COLOR}`
+      }}>
+        <div 
+        onClick={() => history.goBack()}
+        style={{
+          paddingRight: 10,
+          paddingLeft: 10,
+          zIndex: 1
+        }}>
+          <IoChevronBackSharp size={32}/>
+        </div>
+        
+        <div style={{
+          position: 'absolute',
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          zIndex: 0
+        }}>
+          <h1 style={{
+            margin: 0,
+            fontSize: 20,
+            fontWeight: 500,
+            textAlign: 'center',
+          }}>Create Store</h1>
+        </div>
+      </div>
 
+      <div style={{
+        top: HEADER_HEIGHT,
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0
+      }}>
+        <EditAddressView/>
+      </div>
     </div>
   )
 }
