6dd5b833374e7424ce137dd9953a960300096ca9
Stanley <satyafariznur@gmail.com>
2021-07-31 15:45:36 +0700

    add validation

--- src/Mobile/Screens/CreateProduct/CreateProductScreen.js
+++ src/Mobile/Screens/CreateProduct/CreateProductScreen.js
@@ -7,15 +7,91 @@ import useAppContext from '../../hooks/useAppContext'
 import formatCurrency from '../../../helpers/formatCurrency'
 import { useRef, useEffect, useState } from 'react'
 import Link from '../../Components/Link'
-import { Button, TextField } from '@material-ui/core'
+import { Button, TextField, InputAdornment } from '@material-ui/core'
 import Carousel from '@brainhubeu/react-carousel'
 import '@brainhubeu/react-carousel/lib/style.css'
+import Validator from '../../../helpers/validator'
 
 const Component = props => {
   const { category } = props
+  const _isMounted = useRef(true)
   const scrollRef = useRef()
   const [showHeader, setShowHeader] = useState(false)
-  const { history } = useAppContext()
+  const { history, environment } = useAppContext()
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+  const [desc, setDesc] = useState('')
+  const [specs, setSpecs] = useState(category.specFields.reduce((obj, currentVal) => {
+    obj[currentVal.attribute.id] = ''
+    return obj
+  }, {}))
+  const [validation, setValidation] = useState({ isValid: false })
+  const [loading, setLoading] = useState(false)
+
+  const _setSpecs = field => e => {
+    const value = e.target.value.trimLeft()
+    setSpecs(prev => ({ ...prev, [field.attribute.id]: value }))
+  }
+
+  const _setPrice = (e) => {
+    const { value } = e.target
+    const allowedChars = '0123456789'
+    if(value !== '0' && (value === '' || allowedChars.includes(value[value.length - 1])))
+      setPrice(value)
+  }
+
+  const isValid = () => {
+    const specsRules = category.specFields.reduce((rules, currentVal) => {
+      if(currentVal.isRequired) {
+        rules.push({
+          field: currentVal.attribute.id,
+          method: Validator.isEmpty,
+          validWhen: false,
+          message: 'This field is required.'
+        })
+      }
+      
+      return rules
+    }, [])
+
+    const validator = new Validator([
+      ...specsRules,
+      {
+        field: 'name',
+        method: Validator.isEmpty,
+        validWhen: false,
+        message: 'This field is required.'
+      },
+      {
+        field: 'price',
+        method: Validator.isEmpty,
+        validWhen: false,
+        message: 'This field is required.'
+      },
+      {
+        field: 'desc',
+        method: Validator.isEmpty,
+        validWhen: false,
+        message: 'This field is required.'
+      }
+    ])
+
+    const validation = validator.validate({
+      ...specs,
+      name,
+      price,
+      desc
+    })
+
+    setValidation(validation)
+    return validation.isValid
+  }
+
+  const create = () => {
+    if(isValid() && !loading) {
+      
+    }
+  }
 
   useEffect(() => {
     scrollRef.current.onscroll = () => {
@@ -122,25 +198,43 @@ const Component = props => {
             variant="outlined"
             label="Product Name"
             fullWidth
+            value={name}
+            onChange={e => setName(e.target.value.trimLeft())}
             style={{
               marginTop: 10,
               marginBottom: 10
             }}
+            error={validation?.name?.isInvalid}
+            helperText={validation?.name?.message}
           />
 
           <TextField
             variant="outlined"
             label="Product Price"
             fullWidth
+            value={price}
+            onChange={_setPrice}
             style={{
               marginTop: 10,
               marginBottom: 10
             }}
+            InputProps={{
+              startAdornment: <InputAdornment position="start">Rp</InputAdornment>
+            }}
+            inputProps={{
+              pattern: "[0-9]*",
+              type: "text",
+              inputMode: "numeric"
+            }}
+            error={validation?.price?.isInvalid}
+            helperText={validation?.price?.message}
           />
 
           <TextField
             variant="outlined"
             label="Product Description"
+            value={desc}
+            onChange={e => setDesc(e.target.value.trimLeft())}
             multiline
             rows="8"
             fullWidth
@@ -148,6 +242,8 @@ const Component = props => {
               marginTop: 10,
               marginBottom: 10
             }}
+            error={validation?.desc?.isInvalid}
+            helperText={validation?.desc?.message}
           />
 
           <h3 style={{ margin: '10px 0'}}>Specifications</h3>
@@ -159,10 +255,14 @@ const Component = props => {
                 variant="outlined"
                 label={field.attribute.name}
                 fullWidth
+                value={specs[field.attribute.id]}
+                onChange={_setSpecs(field)}
                 style={{
                   marginTop: 10,
                   marginBottom: 10
                 }}
+                error={validation[field.attribute.id]?.isInvalid}
+                helperText={validation[field.attribute.id]?.message}
               />
             )
           })}
@@ -175,6 +275,7 @@ const Component = props => {
               fontTransform: 'none',
               marginTop: 10
             }}
+            onClick={create}
           >
             Create
           </Button>
