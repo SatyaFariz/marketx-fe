4cd6f2fcbfcffa4a4e61f46785d0bebd075cc5e4
Stanley <satyafariznur@gmail.com>
2021-08-14 21:27:03 +0700

    fix

--- src/Mobile/Screens/Store/SelectCategoryView.js
+++ src/Mobile/Screens/Store/SelectCategoryView.js
@@ -43,7 +43,12 @@ const Component = props => {
         </div>
       </div>
       <div style={{
-        marginTop: HEADER_HEIGHT
+        top: HEADER_HEIGHT,
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0,
+        overflow: 'auto'
       }}>
         <List
         >
