e3de003bc14b0525cc941bd139b9d205bc9ce592
Stanley <satyafariznur@gmail.com>
2021-08-23 14:50:20 +0700

    add header

--- src/Mobile/Screens/FAQ/FAQScreen.js
+++ src/Mobile/Screens/FAQ/FAQScreen.js
@@ -1,10 +1,41 @@
 import graphql from 'babel-plugin-relay/macro'
 import { createFragmentContainer } from 'react-relay'
+import { HEADER_HEIGHT, HEADER_BORDER_BOTTOM_COLOR, DIVIDER_COLOR } from '../../Constants'
+import BackButton from '../../Components/BackButton'
 
 const Component = props => {
   return (
     <div>
-
+      <div style={{
+        height: HEADER_HEIGHT,
+        backgroundColor: 'white',
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        position: 'sticky',
+        top: 0,
+        zIndex: 9999,
+        borderBottom: `1px solid ${HEADER_BORDER_BOTTOM_COLOR}`
+      }}>
+        <BackButton/>
+        
+        <div style={{
+          position: 'absolute',
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          zIndex: 0
+        }}>
+          <h1 style={{
+            margin: 0,
+            fontSize: 20,
+            fontWeight: 500,
+            textAlign: 'center',
+          }}>FAQ</h1>
+        </div>
+      </div>
     </div>
   )
 }
