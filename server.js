7895750ee898c32cc5ad0e931e6dc5ee50e5380e
Stanley <satyafariznur@gmail.com>
2021-08-26 00:09:41 +0700

    serve react build using node.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static('build'))
+
+app.disable('x-powered-by')
+app.use('*', express.static(path.join(__dirname, 'build')))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
