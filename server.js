ca2b7dd937d1efcdc5724ffe0a07ff4962fc97f1
Stanley <satyafariznur@gmail.com>
2021-08-26 18:24:09 +0700

    dockerized

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 5000
+const port = parseInt(process.env.PORT, 10)
 
 app.use(express.static('build'))
 
