0a3ab891c89d71a0495f9408848d611186584b15
Stanley <satyafariznur@gmail.com>
2021-08-28 23:09:52 +0700

    ssr all routes

--- server.js
+++ server.js
@@ -12,55 +12,58 @@ import App  from './src/App'
 import createRelay from './src/Mobile/createRelay'
 import router from './src/Mobile/router'
 import qs from 'query-string'
+import routes from './src/Mobile/Screens'
 
 const port = parseInt(process.env.PORT || 5000, 10)
 
 app.disable('x-powered-by')
 
-app.get('/', async (req, res) => {
-  console.log('URL', req.url)
-  const parts = req.url.split('?')
-  const pathname = parts[0]
-  const queryParams = parts[1] ? qs.parse(parts[1]) : {}
-  const indexFile = path.resolve('./build/index.html')
-  const page = await router.resolve({
-    query: queryParams,
-    pathname,
-    fetchQuery: (query, variables) => {
-      return new Promise(async (resolve) => {
-        const res = await axios.post("http://192.168.1.74:4000/graphql", 
-          {
-            query: query.params.text,
-            variables
-          }, 
-          {
-            headers: {}
-          }
-        )
-        resolve(res.data.data)
-      })
-    }
-  })
-  
-  const html = ReactDOMServer.renderToString(
-    <App 
-      history={createHistory()} 
-      createRelay={createRelay} 
-      initialData={page.data}
-      initialComponent={page.component}
-    />
-  )
-  
-  fs.readFile(indexFile, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Something went wrong:', err)
-      return res.status(500).send('Oops, better luck next time!')
-    }
-    return res.send(
-      data.replace('<div id="root"></div>', `<div id="root" class="root">${html}</div>`)
+routes.forEach(route => {
+  app.get(route.path, async (req, res) => {
+    const parts = req.url.split('?')
+    const pathname = parts[0]
+    const queryParams = parts[1] ? qs.parse(parts[1]) : {}
+    const indexFile = path.resolve('./build/index.html')
+    const page = await router.resolve({
+      query: queryParams,
+      pathname,
+      fetchQuery: (query, variables) => {
+        return new Promise(async (resolve) => {
+          const res = await axios.post("http://192.168.1.74:4000/graphql", 
+            {
+              query: query.params.text,
+              variables
+            }, 
+            {
+              headers: {}
+            }
+          )
+          resolve(res.data.data)
+        })
+      }
+    })
+    
+    const html = ReactDOMServer.renderToString(
+      <App 
+        history={createHistory()} 
+        createRelay={createRelay} 
+        initialData={page.data}
+        initialComponent={page.component}
+      />
     )
+    
+    fs.readFile(indexFile, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Something went wrong:', err)
+        return res.status(500).send('Oops, better luck next time!')
+      }
+      return res.send(
+        data.replace('<div id="root"></div>', `<div id="root" class="root">${html}</div>`)
+      )
+    })
   })
 })
+
 
 // app.use('*', express.static(path.join(__dirname, 'build')))
 app.use(express.static('build'))
