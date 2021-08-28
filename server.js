78f3abd26063d08a7b24bea2b584b6526d6f1926
Stanley <satyafariznur@gmail.com>
2021-08-28 22:42:05 +0700

    fix

--- server.js
+++ server.js
@@ -11,35 +11,22 @@ import { createMemoryHistory as createHistory } from 'history'
 import App  from './src/App'
 import createRelay from './src/Mobile/createRelay'
 import router from './src/Mobile/router'
+import qs from 'query-string'
 
 const port = parseInt(process.env.PORT || 5000, 10)
-// app.use(express.static('build'))
 
 app.disable('x-powered-by')
 
 app.get('/', async (req, res) => {
-  // const app = ReactDOMServer.renderToString(<App history={createHistory()} createRelay={createRelay}/>)
-  // const indexFile = path.resolve('./build/index.html')
-  // console.log(req.url)
-  // // console.log('App', app)
-  // fs.readFile(indexFile, 'utf8', (err, data) => {
-  //   if (err) {
-  //     console.error('Something went wrong:', err)
-  //     return res.status(500).send('Oops, better luck next time!')
-  //   }
-  //   return res.send(
-  //     data.replace('<div id="root"></div>', `<div id="root" class="root">${app}</div>`)
-  //   )
-  // })
+  console.log('URL', req.url)
+  const parts = req.url.split('?')
+  const pathname = parts[0]
+  const queryParams = parts[1] ? qs.parse(parts[1]) : {}
   const indexFile = path.resolve('./build/index.html')
   const page = await router.resolve({
-    pathname: req.url,
-    // fetchQuery: (query, variables) => {
-    //   return new Promise(async (resolve) => {
-    //     resolve({ data: {}})
-    //   })
-    // }
-    fetchQuery: (query, variables) => {console.log(query, variables)
+    query: queryParams,
+    pathname,
+    fetchQuery: (query, variables) => {
       return new Promise(async (resolve) => {
         const res = await axios.post("http://192.168.1.74:4000/graphql", 
           {
@@ -54,8 +41,7 @@ app.get('/', async (req, res) => {
       })
     }
   })
-  console.log('Page', page)
-  // const html = ReactDOMServer.renderToString(page.component)
+  
   const html = ReactDOMServer.renderToString(
     <App 
       history={createHistory()} 
@@ -64,7 +50,7 @@ app.get('/', async (req, res) => {
       initialComponent={page.component}
     />
   )
-  // console.log('HTML', html)
+  
   fs.readFile(indexFile, 'utf8', (err, data) => {
     if (err) {
       console.error('Something went wrong:', err)
