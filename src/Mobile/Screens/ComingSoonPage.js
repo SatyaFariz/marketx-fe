3b39605b7378635f2a4b4df913b2c5b6eb933401
Satya Fariz <satyafariznur@gmail.com>
2021-11-19 21:21:05 +0700

    change style

--- src/Mobile/Screens/ComingSoonPage.js
+++ src/Mobile/Screens/ComingSoonPage.js
@@ -1,30 +1,27 @@
 import App from '../../app.json'
 import Color from '../Constants/Color'
+import Logo from '../../assets/marketx_logo.jpg'
 
-const Dot = (props) => {
-  const { size } = props
-  const diameter = size === 'small' ? 20 : (size === 'medium' ? 50 : 75)
-  return (
-    <div style={{
-      position: 'absolute',
-      backgroundColor: Color.primary,
-      height: diameter,
-      width: diameter,
-      borderRadius: '50%',
-      boxShadow: `0 0 3px 7px ${Color.primary}`,
-      ...props.style
-    }}/>
-  )
-}
+const thisYear = new Date().getFullYear()
 
 const Component = () => {
   return (
     <div style={{
       display: 'flex',
-      justifyContent: 'center',
+      flexDirection: 'column',
       alignItems: 'center',
       height: '100vh'
     }}>
+      <div style={{
+        marginTop: 25
+      }}>
+        <img
+          src={Logo}
+          style={{
+            height: 80
+          }}
+        />
+      </div>
       <div style={{
         display: 'flex',
         flexDirection: 'column',
@@ -32,7 +29,8 @@ const Component = () => {
         justifyContent: 'center',
         width: 600,
         textAlign: 'center',
-        zIndex: 999999
+        zIndex: 999999,
+        flexGrow: 1
       }}>
         <h1 style={{
           margin: 0,
@@ -46,68 +44,16 @@ const Component = () => {
         </p>
       </div>
 
-      <Dot
-        size='medium'
-        style={{
-          bottom: 10,
-          left: 0,
-          transform: 'translateX(20vw)'
-        }}
-      />
-
-      <Dot
-        size='medium'
-        style={{
-          bottom: 15,
-          left: 0,
-          transform: 'translateX(85vw)'
-        }}
-      />
-
-      <Dot
-        size='big'
-        style={{
-          top: 0,
-          right: 0,
-          transform: 'translate(50%, 45vh)'
-        }}
-      />
-
-      <Dot
-        size='big'
-        style={{
-          top: 0,
-          left: 0,
-          transform: 'translate(40vw, -50%)'
-        }}
-      />
-
-      <Dot
-        size='small'
-        style={{
-          top: 0,
-          left: 0,
-          transform: 'translate(20vw, 60vh)'
-        }}
-      />
-
-      <Dot
-        size='small'
-        style={{
-          top: 0,
-          left: 0,
-          transform: 'translate(70vw, 85vh)'
-        }}
-      />
-
-      <Dot
-        size='small'
-        style={{
-          top: 0,
-          left: 0,
-          transform: 'translate(75vw, 50vh)'
-        }}
-      />
+      <div style={{
+        backgroundColor: Color.primary,
+        width: '100%',
+        paddingTop: 20,
+        paddingBottom: 20,
+        display: 'flex',
+        justifyContent: 'center'
+      }}>
+        <span style={{ color: 'white' }}>Copyright © {App.year_founded}{App.year_founded === thisYear ? '' : ` - ${thisYear}`} {App.name}</span>
+      </div>
     </div>
   )
 }
