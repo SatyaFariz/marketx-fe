1cd1d0b70f58e2f0cd9a531329c7359b3346b90e
Satya Fariz <satyafariznur@gmail.com>
2021-12-20 23:19:46 +0700

    optimize image compression

--- src/helpers/compressImage.js
+++ src/helpers/compressImage.js
@@ -1,36 +1,23 @@
-import { fromImage } from 'imtool'
-
-const megabytes = 1000000
-
-const conversion = [
-  [10, 0.06],
-  [9, 0.07],
-  [8, 0.082],
-  [7, 0.08],
-  [6, 0.1],
-  [5, 0.2],
-  [4.5, 0.35],
-  [4, 0.4],
-  [3, 0.5],
-  [2, 0.6],
-  [1, 0.65],
-  [0.5, 0.65],
-  [0, 0.65]
-]
+import Compressor from 'compressorjs'
 
 const compressImage = async (file) => {
-  const { size } = file
-  let toQuality = 0.4
-  for(let i = 0; i < conversion.length; i++) {
-    const [mulitiplier, quality] = conversion[i]
-    if(size > (mulitiplier * megabytes)) {
-      toQuality = quality
-      break 
-    }
-  }
+  const image = await new Promise(resolve => {
+    new Compressor(file, {
+      // quality: 0.8, default 0.92
+      maxHeight: 1000,
+      maxWidth: 1000,
+  
+      // The compression process is asynchronous,
+      // which means you have to access the `result` in the `success` hook function.
+      success(result) {
+        resolve(result)
+      },
+      error(err) {
+        console.log(err.message)
+      },
+    })
+  })
 
-  const tool = await fromImage(file)
-  const image = await tool.quality(toQuality).toFile(file.name)
   image.preview = URL.createObjectURL(image)
   return image
 }
